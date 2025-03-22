42bb8220a7aed2f945deef23ae496c18aea8fa87
Qui Nguyen <qui@gmail.com>
2025-03-22 00:34:37 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 import "./App.css";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import { useEffect } from "react";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+
 function App() {
-  return <div>My App</div>;
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5001", // Replace with your API server
+        changeOrigin: true,
+        secure: false, // Set to true if using HTTPS and a valid certificate
+        rewrite: (path) => path.replace(/^\/api/, ""), // Optional: remove '/api' prefix
+      },
+    },
+  },
+});
